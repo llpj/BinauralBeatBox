fc6f8075eb7c8ebce85448640908ec491e0dda72
Magnus Bruehl <mblmg@gmx.de>
2012-04-15 20:18:30 +0200

    Creation of project, container classes partially implemented

---
+++ src/com/helit/binbox/container/Segment.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.helit.binbox.container;
+
+/**
+ * @author AI10MABR
+ *
+ */
+public class Segment {
+	private int duration;
+	private BinauralBeat beat;
+	
+	public int getDuration() {
+		return duration;
+	}
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+	public BinauralBeat getBeat() {
+		return beat;
+	}
+	public void setBeat(BinauralBeat beat) {
+		this.beat = beat;
+	}
+
+}
---
+++ src/com/helit/binbox/container/Session.java
@@ -0,0 +1,76 @@
+package com.helit.binbox.container;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * @author Magnus Brühl
+ *
+ */
+public class Session implements Serializable {
+
+	//Attributes
+	private static final long serialVersionUID = 3318158115697418966L;
+	private String Hintergrundklang; //path to background noise
+	private ArrayList<Segment> segments;
+	private int duration;
+	
+	//Constructors
+	public Session(){
+		//TODO: Add correct path to default background noise.
+		Hintergrundklang = "Path to default background noise.";
+		segments = new ArrayList<Segment>();
+		duration = calcDuration();
+	}
+	public Session(String Hintergrundklang, ArrayList<Segment> segments){
+		this.Hintergrundklang = Hintergrundklang;
+		this.segments = segments;
+	}
+	public Session(String Hintergrundklang, Segment segment){
+		this.Hintergrundklang = Hintergrundklang;
+		this.addSegment(segment);
+	}
+	
+	// Getters and setters
+	public void setHintergrundklang(String hintergrundklang) {
+		Hintergrundklang = hintergrundklang;
+	}
+	public String getHintergrundklang() {
+		return Hintergrundklang;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+	public int getDuration() {
+		return duration;
+	}
+	
+	// Calculations and methods
+	public void addSegment(Segment segment){
+		this.segments.add(segment);
+	}
+	public void removeSegment(int position){
+		this.segments.remove(position);
+	}
+	/**
+	 * Iterates through the list of segments and adds up the durations
+	 * @return the duration of the complete Session
+	 */
+	private int calcDuration(){
+		int lDuration = 0;
+		if(this.segments.isEmpty()){
+			return lDuration;
+		}else{
+			// iterate through the list of segments and add the durations
+			Iterator<Segment> itr = this.segments.iterator();
+			while(itr.hasNext()){
+				lDuration = itr.next().getDuration();
+			}
+			return lDuration;
+		}
+	}
+	
+
+}
