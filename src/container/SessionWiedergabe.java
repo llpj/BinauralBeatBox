5619bb59e88d215e71aeb3e872d5daba98c3fb14
ruclip <boris.beck.de@gmail.com>
2012-04-16 17:39:50 +0200

    SessionWiedergabe editiert

--- src/container/SessionWiedergabe.java
+++ src/container/SessionWiedergabe.java
@@ -1,5 +1,55 @@
 package container;
 
+/**
+ * @author Boris Beck
+ * @return This class load and play a Session
+ *
+ */
+
 public class SessionWiedergabe {
+	
+	// define variables
+	private int currentduration = 0;
+	private int currentfrequenz;
+	private Session current_session;
+	private int volumn;
+	private int balance;
+	
+	public Session loadSession() {
+		// TODO load session
+		return current_session;
+	}
+	
+	public void playSession() {
+		// TODO play Session
+	}
+	
+	public void pauseSession() {
+		// TODO pause
+	}
+	
+	public void stopSession() {
+		// TODO stop Session
+	}
+	
+	public int getcurrentduration() {
+		return currentduration;
+	}
+	
+	public void setcurrentduration(int currentdurration) {
+		this.currentduration = currentdurration;
+	}
+	
+	private void transition(int currentfrequenz, int beginfreq) {
+		// TODO Uebergang zwischen den Segmenten berechnen
+	}
+	
+	private void changeVolumn(int volumn) {
+		this.volumn = volumn;
+	}
+	
+	private void changeBalance(int balance) {
+		this.balance = balance;
+	}
 
 }
