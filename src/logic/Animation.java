a37264309d72156f94a5ae564e8de92023556459
seven89 <fas.schaefer@gmail.com>
2012-04-17 12:31:00 +0200

    test

---
+++ src/logic/Animation.java
@@ -0,0 +1,26 @@
+package logic;
+
+import java.awt.Graphics2D;
+
+import container.Session;
+
+abstract class Animation {
+	
+	private int [] freq = new int[3];
+	
+	public int [] getFreq ()
+	{
+		return freq;
+	}
+	public void setFreq (int [] freq)
+	{
+		this.freq = freq;
+	}
+	
+	public abstract void init (Session session);
+	public abstract boolean pause (); //"pause" an Stelle von "break", da Java bereits break benutzt
+	public abstract boolean finish ();
+	public abstract void setHandle (Graphics2D animationpnl); //Klasse Graphics aus java.awt.* könnte auch verwendet werden
+
+
+}
