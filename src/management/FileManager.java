79ce34fdc1f2cbcdc00a0bc9522b1bf963c2ca09
Magnus Bruehl <mblmg@gmx.de>
2012-04-18 21:27:23 +0200

    FileManager>exportAsWav(). Kann noch nicht funktionieren!

--- src/management/FileManager.java
+++ src/management/FileManager.java
@@ -3,9 +3,20 @@
  */
 package management;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+
+import com.sun.media.sound.WaveFileWriter;
+
 import container.Category;
+import container.Session;
 
 /**
  * Liest und schreibt XML-Dateien. Exportiert komplette Sessions als WAVE-Datei.
@@ -18,6 +29,7 @@ import container.Category;
 public class FileManager {
 	// Attribute
 	private ArrayList<Category> categories;
+	private Session activeSession;
 
 	// Konstruktor
 	public FileManager() {
@@ -33,6 +45,14 @@ public class FileManager {
 		this.categories = categories;
 	}
 
+	public void setActiveSession(Session activeSession) {
+		this.activeSession = activeSession;
+	}
+
+	public Session getActiveSession() {
+		return activeSession;
+	}
+
 	// Weitere Methoden
 	public void addCategory(Category category) {
 		this.categories.add(category);
@@ -40,6 +60,27 @@ public class FileManager {
 
 	public void removeCategory(int index) {
 		this.categories.remove(index);
+	}
+
+	/**
+	 * Oh Shit, das wird schwierig. Wir muessen die Audioinformationen aus der
+	 * Session irgendwie auf eine DataLine schreiben, damit daraus ein
+	 * AudioInputStream generiert werden kann, der dann in ein WaveFile
+	 * exportiert werden kann. Damn!
+	 */
+	public void exportAsWav() {
+		// Hole Audioinformationen, um einen AudioInputStream zu generieren
+		AudioInputStream audioInputStream = new AudioInputStream(targetDataLine);
+		// Neue WAVE-Datei im Filesystem erstellen
+		File fileOut = new File("test.wav"); // TODO: Hier muss der Pfad rein,
+												// den der User spezifiziert
+		WaveFileWriter writer = new WaveFileWriter();
+		// Check, ob das Hostsystem ueberhaupt WAVE-Dateien schreiben kann.
+		if (AudioSystem.isFileTypeSupported(AudioFileFormat.Type.WAVE,
+				audioInputStream)) {
+			writer.write(audioInputStream, AudioFileFormat.Type.WAVE, fileOut);
+		}
+
 	}
 
 }
