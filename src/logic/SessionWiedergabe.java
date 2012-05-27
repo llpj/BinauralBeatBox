fd8549a9d6d6a09e44d768addca371f3d1e33505
Felix S. Pistorius <mail@felix-pistorius.de>
2012-05-28 00:47:10 +0200

    neue SessionWiedergabe

--- src/logic/SessionWiedergabe.java
+++ src/logic/SessionWiedergabe.java
@@ -3,305 +3,126 @@ package logic;
 import java.io.File;
 import java.io.IOException;
 
-import container.BinauralBeat;
-import container.Segment;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 import container.Session;
 
-// Java Sound API
-import javax.sound.sampled.*;
 
-/**
- * @author Boris Beck
- * @return This class load and play a Session
- * 
- */
 
-public class SessionWiedergabe {
+public class SessionWiedergabe implements Runnable{
 
-	private Session session;
-	private long cuDuration = 0;
-	float balc1;
-	float balc2;
-	private AudioFormat playme;
-	private byte sampleSize;
-	private byte[] totalBeat;
-	private File file;
-	private Clip clip1 = null;
-	private Clip clip2 = null;
-	private AudioInputStream ais = null;
-	private AudioInputStream ais2 = null;
-	private SourceDataLine sourceDataLine;
-	private SessionWiedergabe sw;
-	private byte[] data;
+	private Session				session;
+	private AudioFormat			beatFormat;
+	private double				posX;			//Zaehlvariable fuer die Berechnung des Tons
+	private double				stepWidth;
+	private int					sample_max_value;
+	private SourceDataLine		beatLine;
+	private DataLine.Info		beatInfo;
+	private AudioInputStream	audioInputStream;
+	
+	private Thread	t;
+	private boolean pause;
 
-	// Testvariablen
-	static int i = 0;
+	private final static int BUFFER = 4096;
+
 
 	public SessionWiedergabe(Session session) {
 		this.session = session;
-
-		AudioFormat playme = getAudioFormat();
-		// byte sampleSize = (byte) (playme.getSampleSizeInBits() / 8);
-
-		// totalBeat = new byte[(int) playme.getSampleRate() * sampleSize *
-		// session.getDuration()];
-		// transition();
-	}
-
-	/**
-	  * public static void playSession(int freqLinks, int  freqRechts, int freqDauer)
-	  * 
-	  * @param	static int j=1; freqLinks:  linke Frequenz. Wenn 0, dann kein Ton 
-	  * @param freqRechts: rechte Frequenz. Wenn 0, dann kein Ton
-	  * @param freDsuer: Frequennz Wiederholungen
-	  * 
-	  * mit Hilfe von c.getMicrosecondPosition() ist es moeglich, die aktuelle Zeit zu bestimmen
-	  * 
-	  */
-	public void playSession() {
-		i++;
-		System.out.println("Funktion aufgerufen: " + i);
-
-		// Teste ob Session ergolgreich geladne wurde
-		try {
-			if (session != null) {
-				int duration = session.getDuration();
-				System.out
-						.println("Ausgewaehlte Session in den PLayer geladen");
-			} else
-				System.err
-						.println("SessionWiedergabe: Keine Session vorhanden.");
-		} catch (Exception e) {
-			System.err.println(e.getStackTrace());
-		}
-
-		AudioFormat playme = getAudioFormat();
-
-		// // Hintergrundmusik (clip1)
-		file = new File(session.getHintergrundklang());
-
-		try {
-			clip1 = AudioSystem.getClip();
-			ais = AudioSystem.getAudioInputStream(file);
-			clip1.open(ais);
+		
+		this.posX		= 0;
+		this.pause		= false;
+		this.beatFormat 	= new AudioFormat(44100, 16, 1, true, false);
+		this.stepWidth	= (2 * Math.PI) / beatFormat.getSampleRate();
+		this.sample_max_value = (int) Math.pow(2, beatFormat.getSampleSizeInBits()) / 2 - 1;
+		
+		
+		beatInfo = new DataLine.Info(SourceDataLine.class, beatFormat);
+        try {
+			beatLine	= (SourceDataLine) AudioSystem.getLine(beatInfo);
 		} catch (LineUnavailableException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (UnsupportedAudioFileException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public void playSession() {
+		t = new Thread(this);
+		t.start();
+	}
+	
+	public void continueSession()	{	pause = false; }
+	
+	public void pauseSession()		{ pause = true; }
 
-		clip1.loop(-1);
-
-		// Frequenzton (clip2)
-
+	
+	@Override
+	public void run() {
 		try {
-			clip2 = (Clip) AudioSystem.getLine(new Line.Info(Clip.class));
-		} catch (LineUnavailableException e2) {
-			// TODO Auto-generated catch block
-			e2.printStackTrace();
+			beatLine.open( beatFormat, 2*BUFFER );
 		}
-
-		// Loop ueber alle Segmente
-		for (int curSeg = 0; curSeg < session.getNumerOfSegments(); curSeg++) {
-
-			// Hole das aktuelle Segment
-			Segment activeSegment = session.getSegments().get(curSeg);
-			int freq1 = activeSegment.getBeat().getFreq1_start();
-			int freq2 = activeSegment.getBeat().getFreq2_start();
-
-			System.out.println("Links: " + freq1 + " Rechts: " + freq2);
-
-			// generiere Stereo Frequenz
-			byte[] data = getStereoSinusTone(freq1, freq2, playme, 10);
-
-			try {
-				clip2.open(playme, data, 0, data.length);
-			} catch (LineUnavailableException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
+		catch (LineUnavailableException e) { e.printStackTrace(); }
+		
+		beatLine.start();
+		
+		while(posX < session.getDuration() ) {
+			
+			synchronized (this) {
+				while(pause) {
+					try {
+						wait(1);
+					} catch (InterruptedException e) { e.printStackTrace(); }
+				}
 			}
-			System.out.println(session.getDuration());
-
-			clip2.loop(activeSegment.getDuration());
-
-			// clip2.loop(5);
-
-			// Warteschleife
-			long j = 10000000;
-			for (int i = 0; i < j; i++) {
-				System.out.println(i);
-			}
-
-			clip2.stop();
-			clip2.close();
-			System.out.println("Loop Ende");
-
-			// session.getSegments() gibt ne arraylist zurück
-			// und mit session.getSegments().get(segmentNumber) kriegst du ein
-			// segment
-
+			
+			
+			byte[] data = new byte[BUFFER];
+			int numBytesRead = getStereoTon(data, BUFFER);
+			
+			if(numBytesRead == 0) break;
+			beatLine.write(data, 0, numBytesRead);
 		}
-
-		// Balance geandert (damit clip1 leiser ist als Clip2)
-		FloatControl gainControl = (FloatControl) clip1
-				.getControl(FloatControl.Type.MASTER_GAIN);
-		gainControl.setValue(-20); // veringert / erhoeht die Lautsärke um x
-									// Decibel
-
-		System.out.println("Ende...");
-	}
-
-	// Berechnung, um die Frequenzen auf die Boxen zu verteilen
-	public byte[] getStereoSinusTone(int frequency1, int frequency2,
-			AudioFormat af, double duration) {
-		byte sampleSize = (byte) (af.getSampleSizeInBits() / 8);
-		byte[] data = new byte[(int) ((int) af.getSampleRate() * sampleSize * duration)];
-		double stepWidth = (2 * Math.PI) / af.getSampleRate();
-		double x = 0;
-		for (int i = 0; i < data.length; i += sampleSize * 2) {
-
-			int sample_max_value = (int) Math.pow(2, af.getSampleSizeInBits()) / 2 - 1;
-			int value = (int) (sample_max_value * Math.sin(frequency1 * x));
-			for (int j = 0; j < sampleSize; j++) {
-				byte sampleByte = (byte) ((value >> (8 * j)) & 0xff);
-				data[i + j] = sampleByte;
-			}
-
-			value = (int) (sample_max_value * Math.sin(frequency2 * x));
-			for (int j = 0; j < sampleSize; j++) {
-				byte sampleByte = (byte) ((value >> (8 * j)) & 0xff);
-				int index = i + j + sampleSize;
-				data[index] = sampleByte;
-			}
-
-			x += stepWidth;
-		}
-		return data;
-	}
-
-	// Audioformat bestimmen (Durch eigene funktion werden Fehler behoben :)
-	private AudioFormat getAudioFormat() {
-		float sampleRate = 44100;
-		// 8000,11025,16000,22050,44100
-		int sampleSizeInBits = 16;
-		// 8,16
-		int channels = 1;
-		// 1,2
-		boolean signed = true;
-		// true,false
-		boolean bigEndian = false;
-		// true,false
-		// return new AudioFormat( AudioFormat.Encoding.PCM_SIGNED, 8000.0f, 8,
-		// 1, 1,
-		// 8000.0f, false );
-
-		return new AudioFormat(sampleRate, sampleSizeInBits, channels, signed,
-				bigEndian);
-	}
-
-	/**
-	 * pauseSession()
-	 * 
-	 * Session Pausieren. Nur moeglich, wenn die Session schon einmal abgespielt
-	 * worden ist, d.h. wenn die aktuelle Zeit bei 0 steht, dann soll der Pause
-	 * Knopf nix tun
-	 * 
-	 */
-	public void pauseSession() {
-		System.out.println("Pausenbuttontest");
-		cuDuration = clip1.getMicrosecondPosition();
-		System.out.println("Aktuelle Position: " + cuDuration);
-		if (clip1.isRunning()) {
-			clip1.stop();
-			clip2.stop();
-		} else {
-			System.out.println("Pause Fehler: Clip c wurde nicht abgespielt.");
-		}
-	}
-
-	/**
-	 * public static void continueSession()
-	 * 
-	 * setzt die Session Wiedergabe da fort, wo pause gedrueckt worden ist
-	 */
-
-	public long getCuDuration() {
-		return cuDuration;
-	}
-
-	public void continueSession() {
-		System.out.println("Continuebuttontest");
-		clip1.setMicrosecondPosition(cuDuration);
-		clip2.setMicrosecondPosition(cuDuration);
-		System.out.println("Continue at: " + cuDuration);
-		clip1.start();
-		clip2.start();
 	}
 
 	public void stopSession() {
-		System.out.println("Stopbuttontest");
-		if (clip1.isRunning()) {
-			clip1.stop();
-			clip2.stop();
-			clip1.close();
-			clip2.close();
-			cuDuration = 0; // Session auf den Anfang setzen
-		} else {
-			System.out.println("Stop Fehler: Clip wurde nicht abgespielt.");
-		}
-	}
-
-	/**
-	 * 
-	 */
-	private void transition() {
-		int destPos = 0;
-
-		for (Segment s : session.getSegments()) {
-			BinauralBeat b = s.getBeat();
-			// byte[] data = getStereoSinusTone(b.getFreq1_start(),
-			// b.getFreq2_start(), playme, s.getDuration());
-			// System.arraycopy(data, 0, totalBeat, destPos, data.length);
-			// destPos += data.length;
-		}
-	}
-
-	/**
-	 * 
-	 * private void changeVolumn(int volumn)
-	 * 
-	 * @param volumn
-	 *            : floet, Wert um wieviel die Lautstärke veringert (Negativer
-	 *            Wert) oder erhöht werden soll) zB -10.0f veringert die
-	 *            Lautsätke um -10 Decibel
-	 */
-	private void changeVolumn(float volumn) {
-		FloatControl gainControl = (FloatControl) clip1
-				.getControl(FloatControl.Type.MASTER_GAIN);
-		gainControl.setValue(volumn); // veringert / erhoeht die Lautsärke um x
-										// Decibel
+		beatLine.close();
+		beatLine.stop();
+		t.interrupt();
 	}
 	
-	
-	private void changeBalance(float balance, boolean c1c2) { //c1c2 bedeutet, wenn true dann setzen lautsärke von Clip1 um - balance/2 und clip2 um + balance/2, und andersrum	
-		float hier = balance /2;		
-		FloatControl gainControl1 = (FloatControl) clip1.getControl(FloatControl.Type.MASTER_GAIN);
-		FloatControl gainControl2 = (FloatControl) clip2.getControl(FloatControl.Type.MASTER_GAIN);
-				
-		if (c1c2) {
-			balc1 = balc1 - hier;
-			balc2 = balc2 + hier;
-		} else {
-			balc1 = balc1 + hier;
-			balc2 = balc2 - hier;
+	private int getStereoTon(byte[] data, int buffer) {
+		int x = 0;
+		
+		while ( (x < (int)(buffer/4)) && !(posX >= session.getDuration()) ) {
+			byte[] l = getTone( session.getFreqAt(posX, true) );
+			data[x + 0] = l[0];
+			data[x + 1] = l[1];
+			
+			byte[] r = getTone( session.getFreqAt(posX, false) );
+			data[x + 2] = r[0];
+			data[x + 3] = r[1];
+			
+			x += 4;
+			posX += stepWidth;
 		}
 
-		gainControl1.setValue(balc1);
-		gainControl2.setValue(balc2);
+		return x;
 	}
+	
+	private byte[] getTone(double freq) {
+		byte[] data = new byte[2];
+		int value = (int) (sample_max_value * Math.sin(freq * posX));
+		
+		//zerlege value (int) in 2 bytes
+		data[0]		= (byte)  (value & 0xff);
+		data[1]		= (byte) ((value >> 8) & 0xff);
+		
+		return data;
+	}
+
 }
