b2e31a0a8956a5f0f66225ed5791a52f35bd9ae9
seven89 <fas.schaefer@gmail.com>
2012-09-08 14:27:39 +0200

    JUnitAnimation als beispielhafte JUnit-Klasse reduziert

--- src/container/JunitAnimation.java
+++ src/container/JunitAnimation.java
@@ -5,21 +5,16 @@
  * Lessons Learned: JUNIT sollte bereits von Beginn an zum Testen der Funktionen verwendet werden. 
  * Die Verwendung von JUNIT zum Ende eines Softwareprojekts kostet wiederum mehr Aufwand und Zeit. 
  * 
- * Aus diesem Grund sind im Folgenden gegebenenfalls nicht alle möglichen Tests für die
- * Animationsklasse und deren Unterklassen implementiert. 
+ * Aus diesem Grund ist im Folgenden nur ein beispielhafter JUnitTest illustriert 
  */
 
 
 package container;
-import interfaces.Mood;
+
 import gui.MainFrame;
 import junit.framework.*;
 import logic.*;
 
-import java.awt.Color;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 public class JunitAnimation extends TestCase {
 
 	public JunitAnimation(String name){
@@ -31,26 +26,8 @@ public class JunitAnimation extends TestCase {
 		MainFrame mf;
 		final Animation animation;
 		mf = new MainFrame();
-		int resize = 0;
 		int[] freq = { -30, 0, 30 };
-		int num;
-		String input = null;
-		
-		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print("Wählen Sie bitte zwischen folgenden Animationen (1-3):\n 1. " +
-				"Frequenz\n 2. Farbverlauf\n 3. Fraktale\n");
-		try
-		{
-			input = console.readLine();
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-		num = Integer.parseInt(input);
-		switch(num)
-		{
-			case 1: animation = new AnimationFreq(freq, mf.getVirtualizationPnl());
+		animation = new AnimationFreq(freq, mf.getVirtualizationPnl());
 					assertTrue("Die Frequenz beträgt -30, 0, 30",animation.getFreq() == freq);
 					while(animation.isFinished() == false)
 					{
@@ -72,32 +49,6 @@ public class JunitAnimation extends TestCase {
 									animation.getTempo() == 30);
 						}
 					}
-					break;
-			case 2: animation = new FrakFarbverlauf(Mood.BETA, mf.getVirtualizationPnl(), false);
-					while(animation.isFinished() == false)
-					{
-						Color [] c = ((FrakFarbverlauf) animation).getColors();
-						assertTrue("Bei Mood BETA sind diese Farben nicht enthalten", c[0] == Color.yellow.darker() &&
-								c[1] == Color.orange.brighter() && c[2] == Color.red.brighter() &&
-								c[3] == Color.white);
-						assertTrue("Farbverlauf Animation",((FrakFarbverlauf)animation).isFraktal() == false);
-					}
-					break;
-			case 3: animation = new FrakFarbverlauf(Mood.ALPHA, mf.getVirtualizationPnl(), true);
-					while(animation.isFinished() == false)
-					{
-						Color [] c1 = ((FrakFarbverlauf) animation).getColors();
-							assertTrue("Bei Mood ALPHA sind diese Farben nicht enthalten",
-									c1[0] == Color.blue.brighter() && c1[1] == Color.green.darker() &&
-									c1[2] == Color.orange.brighter() && c1[3] == Color.white);
-							assertTrue("Fraktale Animation",((FrakFarbverlauf)animation).isFraktal() == true);
-					}
-					break;
-		}
-		
-		
-		
-		
 	}
 	
 	public void main(String[] args) {
